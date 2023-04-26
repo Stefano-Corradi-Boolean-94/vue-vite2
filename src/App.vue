1db0fe30fe59a4438a413bf8d7a91b59bc4d21f3
ugodeughi <stefano.corradi@gmail.com>
2023-04-26 13:10:46 +0200

    fine LC base componenti

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+import Header from '@/components/Header.vue'
+
+
+export default {
+  name:'App',
+  components:{
+    Header
+  },
+  data() {
+    return {
+      title: 'Ciao Vue - Vite'
+    }
+  },
+}
+</script>
+
+<template>
+
+  <Header />
+
+  <h1>{{ title }}</h1>
+</template>
+
+
+<style scoped>
+  h1{
+    color: yellow;
+  }
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+
+<script>
+export default {
+  name:'Header',
+  data() {
+    return {
+      logo:'LOGO',
+      image: 'vue.svg'
+    }
+  },  
+  methods:{
+    // funzione per gestire le immagini dinamicamente 
+    getImage(img){
+      return new URL(`../assets/${img}`, import.meta.url).href
+    }
+  }
+}
+</script>
+
+
+<template>
+  <nav>
+    <ul>
+      <li><a href="#">{{ logo }} 
+        <!-- immagine da assets  -->
+        <img src="../assets/vue.svg" alt="assets"> 
+        <!-- immagine da public com percorso assoluto partendo da public  -->
+        <img src="/vite.svg" alt="public"> 
+        <!-- immagine dinamica  -->
+        <img :src="getImage(image)" alt="dinamica"> 
+        </a></li>
+      <li><a href="#">link</a></li>
+      <li><a href="#">link</a></li>
+      <li><a href="#">link</a></li>
+    </ul>
+  </nav>
+  
+</template>
+
+<style scoped>
+    nav li{
+      list-style: none;
+      display: inline-block;
+      margin: 0 20px;
+    }
+</style>
